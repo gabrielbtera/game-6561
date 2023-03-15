475fb574d3db94fc1634f7d4d22b749c75714dba
gabrielbtera <gabriel.jesus@dcomp.ufs.br>
2023-03-15 02:38:46 -0300

    integracao com o server

--- testes.py
+++ testes.py
@@ -1,8 +1,9 @@
-# import player3
 import board
 from copy import deepcopy
 import sys
 import time
+
+import random
 
 sys.setrecursionlimit(10000) # Set the recursion limit to 1500 calls
 
@@ -25,42 +26,28 @@ RIGHT = 'R'
 
 
 
-def evaluate(state, player):
+def avaliacao(state: list, player: str) -> int:
     """
     Retorna a pontuação de um determinado estado do jogo para o jogador especificado.
     """
-    score = 0
     zeros = 0
-    for i in range(4):
-        for j in range(4):
-            if state[i][j] != EMPTY:
-                temp = state[i][j]['valor']
-                score += temp
-            else:
-                zeros += 1
-    eq = score - 104976  
-
     score = 0
     for i in range(4):
         for j in range(4):
             # Add points for each tile that is a multiple of 3
             if state[i][j] != EMPTY:
                 if state[i][j]['valor'] % 3 == 0:
-                    score += state[i][j]['valor'] // 3
+                    score += (state[i][j]['valor'] // 3) 
                 # Subtract points for each tile that is not a multiple of 3
                 else:
-                    score -= state[i][j]['valor'] // 3
+                    score -= (state[i][j]['valor'] // 3)
+            else:
+                zeros += 1
+    return score + zeros
 
-    # print(score)
-    return score
 
 
-
-def can_slide(tabuleiro, direcao):
-    """
-    Verifica se a peça na posição (linha_atual, coluna_atual) no tabuleiro pode deslizar na direção especificada.
-    Retorna True se puder deslizar, False caso contrário.
-    """
+def fazSlide(tabuleiro, direcao) -> bool:
 
     if direcao == 'L':
        return board.slideLeftOuRight(tabuleiro, 'L')[1]
@@ -72,50 +59,41 @@ def can_slide(tabuleiro, direcao):
         return board.slideUpDwon(tabuleiro, 'D')[1]
     
 
-
-
-
-def get_valid_moves(state: list, player: str):
+def pegaMovimentosValidos(tabuleiro: list, player: str):
     """
     Retorna uma lista de movimentos válidos para o jogador especificado a partir do estado atual do jogo.
     """
     valid_moves = []
 
-   
-      
-    # if player in [AZUL, CINZA, VERMELHO]:
-    for i in range(4):
-        for j in range(4):
-            if state[i][j] == EMPTY:
-                if player == AZUL:
-                    valid_moves.append((PLACE_BLUE, i, j))
-                elif player == VERMELHO:
-                    valid_moves.append((PLACE_RED, i, j))
-                elif player == CINZA:
-                    valid_moves.append((PLACE_GREY, i, j))
-    # else:
-        
-    if can_slide(state, UP):
+    # for i in range(4):
+    #     for j in range(4):
+    #         if tabuleiro[i][j] == EMPTY:
+    #             if player == AZUL:
+    #                 valid_moves.append((PLACE_BLUE, i, j))
+    #             elif player == VERMELHO:
+    #                 valid_moves.append((PLACE_RED, i, j))
+    #             elif player == CINZA:
+    #                 valid_moves.append((PLACE_GREY, i, j))
+
+    if fazSlide(tabuleiro, UP):
         valid_moves.append((UP,))
-    if can_slide(state, DOWN):
+    if fazSlide(tabuleiro, DOWN):
         valid_moves.append((DOWN,))
-    if can_slide(state, LEFT):
+    if fazSlide(tabuleiro, LEFT):
         valid_moves.append((LEFT,))
-    if can_slide(state, RIGHT):
+    if fazSlide(tabuleiro, RIGHT):
         valid_moves.append((RIGHT,))
     
-   
-
     return valid_moves
 
 
 
-def make_move(state: list, move: tuple):
+def executarMovimentos(tabuleiro: list, move: tuple) -> list:
     """
     Aplica o movimento especificado no estado atual do jogo e retorna o novo estado resultante.
     """
 
-    new_state = deepcopy(state)
+    new_state = deepcopy(tabuleiro)
     if move[0] == PLACE_BLUE:
         new_state[move[1]][move[2]] = {'cor': AZUL, 'valor': 1}
     elif move[0] == PLACE_RED:
@@ -142,81 +120,64 @@ def is_terminal(state):
     Verifica se o jogo terminou a partir do estado atual do jogo.
     Retorna True se o jogo terminou, False caso contrário.
     """
+
+    print(len(pegaMovimentosValidos(state, '')))
+    global tempo_inicial
     # Verifica se o tabuleiro está completamente preenchido
-    if all(cell != EMPTY for row in state for cell in row):
+    if all(cell != EMPTY for row in state for cell in row) and not len(pegaMovimentosValidos(state, '')):
         return True
 
     # Verifica se algum jogador não tem mais peças no tabuleiro
-    if not any(EMPTY in row for row in state):
+    if not any(EMPTY in row for row in state) and not len(pegaMovimentosValidos(state, '')):
         return True
 
     # # Verifica se algum jogador já venceu
     # for player in [BLUE, RED, GREY]:
     #     if evaluate(state, player) == 4:
     #         return True
-    if (time.time() - tempo_inicial  >= 50):
+    if (time.time() - tempo_inicial  >= 6):
+        tempo_inicial = time.time()
         return True
     
 
     # Se nenhuma das condições acima for atendida, o jogo não terminou
     return False
 
-lista = []
 
-def printa(tab):
-    for i in tab:
-        print(i)
 
 
 
 # Define a função minimax
-def minimax(state, depth, alpha, beta, maximizing_player, opc):
+def minimax(tabuleiro, profundidade, alpha, beta, maximizando_player, opc):
     """
     Executa a busca minimax até a profundidade especificada e retorna a melhor jogada possível
     para o jogador atual a partir do estado atual do jogo.
     """
-    if depth == 0 or is_terminal(state):
-        return None, evaluate(state, opc)
+    if profundidade == 0 or is_terminal(tabuleiro):
+        return None, avaliacao(tabuleiro, opc)
     
-   
 
-    if maximizing_player:
+    if maximizando_player:
         max_score = float('-inf')
         best_move = None
-        valids = get_valid_moves(state, opc)
+        valids = pegaMovimentosValidos(tabuleiro, opc)
         for move in valids:
-            new_state = make_move(state, move)
-            printa(state)
-            print()
-            print('íf', move)
-            
-            printa(new_state)
-            print(valids)
-            print()
-
-            _, score = minimax(new_state, depth-1, alpha, beta, False, opc)
+            new_state = executarMovimentos(tabuleiro, move)
+            _, score = minimax(new_state, profundidade-1, alpha, beta, False, opc)
             if score > max_score:
                 max_score = score
                 best_move = move
             alpha = max(alpha, max_score)
             if beta <= alpha:
                 break
-        lista.append((best_move, max_score))
         return best_move, max_score
     else:
         min_score = float('inf')
         best_move = None
-        valids = get_valid_moves(state, opc)
+        valids = pegaMovimentosValidos(tabuleiro, opc)
         for move in valids:
-            new_state = make_move(state, move)
-            printa(state)
-            print()
-            print('else', move)
-           
-            printa(new_state)
-            print(valids)
-            print()
-            _, score = minimax(new_state, depth-1, alpha, beta, True, opc)
+            new_state = executarMovimentos(tabuleiro, move)
+            _, score = minimax(new_state, profundidade-1, alpha, beta, True, opc)
             if score < min_score:
                 min_score = score
                 best_move = move
@@ -228,6 +189,98 @@ def minimax(state, depth, alpha, beta, maximizing_player, opc):
 
     
 
+def coresProximoMovimento(tabuleiro: list, cor: str):
+    movimentosValidos = []      
+    for i in range(4):
+        for j in range(4):
+            if tabuleiro[i][j] == EMPTY:
+                movimentosValidos.append((i, j))
+    
+    cores = {AZUL : [VERMELHO, CINZA], VERMELHO: [CINZA], CINZA: []}
+
+    movimentosEVizinhos = []
+    for movimento in movimentosValidos:
+        vizinhos = []
+        linha, coluna = movimento
+        
+        if linha > 0:
+             vizinhos.append(tabuleiro[linha-1][coluna]) # vizinho acima
+        if linha < 3:
+            vizinhos.append(tabuleiro[linha+1][coluna]) # vizinho abaixo
+        if coluna > 0:
+            vizinhos.append(tabuleiro[linha][coluna-1]) # vizinho à esquerda
+        if coluna < 3:
+            vizinhos.append(tabuleiro[linha][coluna+1]) # vizinho à direita
+        
+        movimentosEVizinhos.append((movimento, vizinhos))
+    
+    opcoesCores = cores[cor]
+
+    final = []
+    for opcaoCor in opcoesCores:
+        for movimentoEVizinhos in movimentosEVizinhos:
+            dic = {'cor': opcaoCor, 'valor': 1}
+            movimento, vizinhos = movimentoEVizinhos
+            
+            if all(dic != dicionario for dicionario in vizinhos):
+                final.append((movimento, dic))
+                break
+    
+    valores = []
+    for validos in final:
+        pos, val = validos
+        movimentosValidos.remove(pos)
+        valores.append(val['cor'])
+    
+    for color in opcoesCores:
+        if color not in valores:
+            mvVal = random.choice(movimentosValidos)
+            final.append((mvVal, {'cor': color, 'valor': 1}))
+
+    return final
+
+
+
+
+
+def melhorLugarCor(tabuleiro: list, cor: str) -> tuple or bool:
+  
+    movimentosValidos = []      
+    for i in range(4):
+        for j in range(4):
+            if tabuleiro[i][j] == EMPTY:
+                if cor == AZUL:
+                    movimentosValidos.append((PLACE_BLUE, i, j))
+                elif cor == VERMELHO:
+                    movimentosValidos.append((PLACE_RED, i, j))
+                elif cor == CINZA:
+                    movimentosValidos.append((PLACE_GREY, i, j))
+
+    def incrementaProximosmovimentos(tab: list, c: str) -> None:
+        movs = coresProximoMovimento(tab, c)
+        for mov in movs:
+            i, j = mov[0]
+            tab[i][j] = mov[1]
+            
+    melhores = []
+    profundidade = len(movimentosValidos)
+    for valid in movimentosValidos:
+        estado = deepcopy(tabuleiro)
+        estado[valid[1]][valid[2]] = {'cor': valid[0], 'valor': 1}
+        incrementaProximosmovimentos(estado, cor)
+        mini_max = minimax(estado, profundidade, float('-inf'), float('inf'),True,  '')
+        melhores.append((mini_max[1],valid))
+    
+    if len(melhores) >  0:
+        maximo = max(melhores, key=lambda x: x[0])
+
+        empates = [i for i in melhores if maximo[0] == i [0]]
+        
+        return  random.choice(empates)[1]
+    else:
+        return False
+
+
 
 tab = [
     [ 0, {'cor': 'C', 'valor' : 1}, 0, {'cor': 'V', 'valor' : 9}],
@@ -238,7 +291,7 @@ tab = [
 
 
 tab1 = [
-    [ {'cor': 'C', 'valor' : 1}, {'cor': 'V', 'valor' : 9},  {'cor': 'C', 'valor' : 1}, {'cor': 'C', 'valor' : 2}],
+    [ {'cor': 'C', 'valor' : 1}, {'cor': 'V', 'valor' : 1},  {'cor': 'C', 'valor' : 1}, {'cor': 'C', 'valor' : 2}],
     [{'cor': 'V', 'valor' : 3}, {'cor': 'A', 'valor' : 1}, {'cor': 'A', 'valor' : 9}, {'cor': 'C', 'valor' : 3} ],
     [{'cor': 'C', 'valor' : 2}, {'cor': 'C', 'valor' : 3}, {'cor': 'V', 'valor' : 3}, {'cor': 'C', 'valor' : 9}],
     [{'cor': 'A', 'valor' : 3}, {'cor': 'V', 'valor' : 1}, {'cor': 'A', 'valor' : 9}, {'cor': 'C', 'valor' : 1}]
@@ -252,28 +305,177 @@ tab2 = [
     ]
 
 tab3 = [
-    [{'cor': 'V', 'valor' : 3}, {'cor': 'A', 'valor' : 2},  {'cor': 'C', 'valor' : 2}, {'cor': 'V', 'valor' : 9}],
-    [{'cor': 'V', 'valor' : 3}, 0, 0,0],
+    [{'cor': 'V', 'valor' : 1}, {'cor': 'A', 'valor' : 2},  {'cor': 'C', 'valor' : 1}, {'cor': 'V', 'valor' : 9}],
+    [{'cor': 'C', 'valor' : 1}, 0, 0,0],
     [0, 0, 0,0],
-    [0, 0, 0,{'cor': 'C', 'valor' : 9}],
+    [0, 0, 0,{'cor': 'C', 'valor' : 1}],
     ]
 
-# get_valid_moves(tab1, 'A')
+tab4 = [
+    [0, 0,  0, 0],
+    [0, 0, 0,0],
+    [0, 0, 0,0],
+    [0, 0, 0,0],
+    ]
 
-print(minimax(tab,2, float('-inf'), float('inf'),True,  'A'))
+print(minimax(tab, 16, float('-inf'), float('inf'),True,  ''))
 
-# print(get_valid_moves(tab3, ''))
-# print(make_move(tab3,('D', )))
+def pegarQuantidadeDeVazios(tabuleiro: list) -> str:
+    cont = 0
+    for i in range(4):
+        for j in range(4):
+            if tabuleiro[i][j] == EMPTY:
+                cont += 1
+    if cont == 16:
+        possibilidades = ['1','2','3', '4']
+        posi = []
+        for i in possibilidades:
+            for j in possibilidades:
+                posi.append(i+j)
+        return random.choice(posi)
+    return ''
 
-# print(get_valid_moves(tab3, ''))
+def getFormataPeca(peca: tuple):
+    return {'cor': peca[0], 'valor': 1}
 
-# print('\n', lista)
+def setNovaPecaTabuleiro(tabuleiro: list, peca: tuple) -> None:
+    _, i, j = peca
+    tabuleiro[i][j] = getFormataPeca(peca)
+    return f'{i+1}{j+1}'
+
+def mainNovaPeca(tabuleiro: list, cor: str) -> str:
+
+    def getFormataPeca(peca: tuple):
+        return {'cor': peca[0], 'valor': 1}
+
+    def setNovaPecaTabuleiro(tabuleiro: list, peca: tuple) -> None:
+        _, i, j = peca
+        tabuleiro[i][j] = getFormataPeca(peca)
+        return f'{i+1}{j+1}'
+
+    melhorLugar = melhorLugarCor(tabuleiro, cor)
+    if melhorLugar:
+        return setNovaPecaTabuleiro(tabuleiro, melhorLugar)
+    else:
+        return 'Quit'
+
+def mainMovimento(tabuleiro: list, opc= ''):
+    PROFUNDIDADE = 10
+    
+    def getMovimento(resultado: tuple) -> tuple:
+        return resultado[0]
+    
+   
+    resultadoMinimax = minimax(tabuleiro, PROFUNDIDADE, float('-inf'), float('inf'),True,  '')
+    movimento = getMovimento(resultadoMinimax)
+    novo_tab =  executarMovimentos(tabuleiro, movimento)
+    for i in len(novo_tab):
+        tabuleiro[i] = novo_tab[i]
+    return movimento[0]
+
+
+
+def getAcao(tipoCor: int) -> str:
+    opcoes = {0: 'B', 
+              1: 'C', 
+              2 : 'D1',
+              3: 'V', 
+              4 : 'D1'
+              }
+    return opcoes[tipoCor]
+
+
+TABULEIRO = [[0,0,0, 0] for _ in range(4)]
+
+def main():
+
+    entrada = sys.stdin.readline()
+    
+    contadorJogadas = 0
+    contadorIteracoes = 0
+    if (entrada.strip() == "A"):
+        
+        while (True):
+            if contadorJogadas > 4:
+                contadorJogadas = 0
+
+            if entrada.strip() == "Quit":
+                break
+            
+            if contadorIteracoes == 999:
+                print('Quit')
+                sys.stdout.flush()
+                break
+
+            if contadorJogadas in [0,1,3]:
+                cor = getAcao(contadorJogadas)
+                posicao = mainNovaPeca(TABULEIRO, cor)
+                print(posicao)
+                sys.stdout.flush()
+                if posicao == 'Quit':
+                    break
+
+            if contadorJogadas in [2,4]:
+                dado = pegarQuantidadeDeVazios(TABULEIRO)
+                if not dado:
+                    print(mainMovimento(TABULEIRO))
+                else:
+                    print()
 
 
 
 
-# Faça algo aqui
-tempo_final = time.time()
+        
 
-intervalo_de_tempo = tempo_final - tempo_inicial
-print(intervalo_de_tempo)
+            print(str(lc))
+
+            sys.stdout.flush()
+
+            entrada = sys.stdin.readline()
+
+            acao = getAcao(entrada)
+
+            contadorJogadas += 1
+            contadorIteracoes += 1
+
+                
+
+
+            
+    else:
+        # Estou jogando como Order
+        memoria = []
+        while (True):
+            if (entrada.strip() == "Quit"):
+                break
+            lc = random.randint(11, 44)
+            # memoria.append(entrada[1]+entrada[2])
+            # Escolha aleatoria de peca existente
+            # random.shuffle(memoria)
+            # peca = memoria[0]
+            # memoria.remove(peca)
+            # Laço para definir local destino
+            # Tem que respeitar horizontal ou vertical
+            # random.shuffle(linhas)
+            # random.shuffle(colunas)
+            # lc = linhas[0]+colunas[0]
+            # while ((lc in memoria) or (lc[0]!=peca[0]) or (lc[1]!=peca[1])):
+            #     random.shuffle(linhas)
+            #     random.shuffle(colunas)
+            #     lc = linhas[0]+colunas[0]
+            # memoria.append(lc)
+            # #Saida da jogada
+            # print(peca+lc)
+            lc = random.randint(11, 44);
+
+            print(lc)
+
+            sys.stdout.flush()
+            # Leitura da jogada do adversario Chaos
+            
+            entrada = sys.stdin.readline()
+
+
+
+# main()
+
--- unificado.py
+++ unificado.py
@@ -1,204 +1,107 @@
+
 import random
+EMPTY = 0
+AZUL  = 'A'
+CINZA = 'C'
+VERMELHO = 'V'
 
-class Node:
-    def __init__(self, state, parent=None):
-        self.state = state
-        self.parent = parent
-        self.children = []
-        self.visits = 0
-        self.value = 0
-    
-    def expand(self):
-        for move in self.state.get_moves():
-            child_state = self.state.make_move(move)
-            child_node = Node(child_state, parent=self)
-            self.children.append(child_node)
-    
-    def select_child(self, exploration_constant=1.4):
-        log_total_visits = math.log(sum(child.visits for child in self.children))
-        best_score = float("-inf")
-        best_child = None
-        for child in self.children:
-            exploitation_score = child.value / child.visits
-            exploration_score = exploration_constant * math.sqrt(log_total_visits / child.visits)
-            uct_score = exploitation_score + exploration_score
-            if uct_score > best_score:
-                best_score = uct_score
-                best_child = child
-        return best_child
-    
-    def update(self, reward):
-        self.visits += 1
-        self.value += reward
-        if self.parent:
-            self.parent.update(reward)
-    
-    def is_leaf(self):
-        return len(self.children) == 0
-    
-class State:
-    def __init__(self, board):
-        self.board = board
-    
-    def get_moves(self):
-        moves = []
-        for i in range(len(self.board)):
-            for j in range(len(self.board[0])):
-                if self.board[i][j] != 0:
-                    continue
-                moves.append((i, j))
-        return moves
-    
-    def make_move(self, move):
-        i, j = move
-        new_board = [row[:] for row in self.board]
-        new_board[i][j] = 1
-        return State(new_board)
-    
-    def is_terminal(self):
-        return self.get_winner() is not None
-    
-    def get_winner(self):
-        for i in range(len(self.board)):
-            row = self.board[i]
-            if row[0] != 0 and row.count(row[0]) == len(row):
-                return row[0]
-        for j in range(len(self.board[0])):
-            column = [self.board[i][j] for i in range(len(self.board))]
-            if column[0] != 0 and column.count(column[0]) == len(column):
-                return column[0]
-        diagonal1 = [self.board[i][i] for i in range(len(self.board))]
-        if diagonal1[0] != 0 and diagonal1.count(diagonal1[0]) == len(diagonal1):
-            return diagonal1[0]
-        diagonal2 = [self.board[i][len(self.board) - i - 1] for i in range(len(self.board))]
-        if diagonal2[0] != 0 and diagonal2.count(diagonal2[0]) == len(diagonal2):
-            return diagonal2[0]
-        if all(self.board[i][j] != 0 for i in range(len(self.board)) for j in range(len(self.board[0]))):
-            return 0
-        return None
-
-class MCTS:
-    def __init__(self, exploration_constant=1.4):
-        self.exploration_constant = exploration_constant
-    
-    def search(self, initial_state, num_simulations):
-        root = Node(initial_state)
-        for i in range(num_simulations):
-            node = root
-            state = initial_state
-            # Select
-            while not node.is_leaf():
-                node = node.select_child(self.exploration_constant)
-                move = node.state.last_move
-                state = state.make_move(move)
-            # Expand
-            if not state.is_terminal():
-                node.expand()
-                # Randomly select a child
-                node = random.choice(node.children)
-                state = state.make_move(node.state.last_move)
-            # Simulate
-            while not state.is_terminal():
-                moves = state.get_moves()
-                move = random.choice(moves)
-                state = state.make_move(move)
-            # Backpropagate
-            winner = state.get_winner()
-            while node is not None:
-                if winner == 0:
-                    reward = 0.5
-                elif winner == node.state.current_player:
-                    reward = 1
-                else:
-                    reward = 0
-                node.update(reward)
-                node = node.parent
-        best_child = max(root.children, key=lambda child: child.visits)
-        return best_child.state.last_move
+# Define as constantes do movimento
+PLACE_BLUE = AZUL
+PLACE_RED = VERMELHO
+PLACE_GREY = CINZA
 
 
-def get_score(state):
-    if state.is_terminal():
-        winner = state.get_winner()
-        if winner == 'A':
-            return (1, -1)
-        elif winner == 'C':
-            return (-1, 1)
-        else:
-            return (0, 0)
-    
-    scores = {'A': 0, 'C': 0}
-    for i in range(len(state.matrix)):
-        for j in range(len(state.matrix[i])):
-            if isinstance(state.matrix[i][j], dict):
-                scores[state.matrix[i][j]['cor']] += state.matrix[i][j]['valor']
-    
-    return (scores['A'], scores['C'])
 
 tab3 = [
-    [{'cor': 'V', 'valor' : 2}, {'cor': 'A', 'valor' : 2},  {'cor': 'C', 'valor' : 2}, {'cor': 'C', 'valor' : 1}],
+    [{'cor': 'V', 'valor' : 2}, 0,  {'cor': 'C', 'valor' : 1}, {'cor': 'V', 'valor' : 9}],
     [{'cor': 'C', 'valor' : 1}, 0, 0,0],
     [0, 0, 0,0],
-    [0, 0, 0,0]
-    ]
-
-initial_state = State(tab3)
-mcts = MCTS(exploration_constant=1.4)
-best_move = mcts.search(initial_state, 1000)
-print("Melhor jogada: ", best_move)
+    [0, 0, 0,{'cor': 'C', 'valor' : 1}]
+]
 
 
 
-def minimax_coop(state, depth, alpha, beta, maximizing_player, is_cooperative, opc):
-    """
-    Executa a busca minimax até a profundidade especificada e retorna a melhor jogada possível
-    para o jogador atual a partir do estado atual do jogo.
-    """
-    if depth <= 0 or is_terminal(state, is_cooperative):
-        return None, evaluate(state, is_cooperative)
 
-    if maximizing_player:
-        max_score = float('-inf')
-        best_move = None
-        valids = get_valid_moves(state, is_cooperative)
-        for move in valids:
-            new_state = make_move(state, move)
-            _, score = minimax(new_state, depth-1, alpha, beta, False, True, )
-            if score > max_score:
-                max_score = score
-                best_move = move
-            alpha = max(alpha, max_score)
-            if beta <= alpha:
+def coresProximoMovimento(tabuleiro: list, cor: str):
+    movimentosValidos = []      
+    for i in range(4):
+        for j in range(4):
+            if tabuleiro[i][j] == EMPTY:
+                movimentosValidos.append((i, j))
+    
+    cores = {AZUL : [VERMELHO, CINZA], VERMELHO: [CINZA], CINZA: []}
+
+    movimentosEVizinhos = []
+    for movimento in movimentosValidos:
+        vizinhos = []
+        linha, coluna = movimento
+        
+        if linha > 0:
+             vizinhos.append(tabuleiro[linha-1][coluna]) # vizinho acima
+        if linha < 3:
+            vizinhos.append(tabuleiro[linha+1][coluna]) # vizinho abaixo
+        if coluna > 0:
+            vizinhos.append(tabuleiro[linha][coluna-1]) # vizinho à esquerda
+        if coluna < 3:
+            vizinhos.append(tabuleiro[linha][coluna+1]) # vizinho à direita
+        
+        movimentosEVizinhos.append((movimento, vizinhos))
+    
+    opcoesCores = cores[cor]
+
+    
+    final = []
+    for opcaoCor in opcoesCores:
+        for movimentoEVizinhos in movimentosEVizinhos:
+            dic = {'cor': opcaoCor, 'valor': 1}
+            movimento, vizinhos = movimentoEVizinhos
+            
+            if all(dic != dicionario for dicionario in vizinhos):
+                final.append((movimento, dic))
                 break
-        return best_move, max_score
-    else:
-        if is_cooperative:
-            # Jogador 2 é cooperativo
-            min_score = float('inf')
-            best_move = None
-            valids = get_valid_moves(state, is_cooperative)
-            for move in valids:
-                new_state = make_move(state, move)
-                _, score = minimax(new_state, depth-1, alpha, beta, True, is_cooperative, opc)
-                if score < min_score:
-                    min_score = score
-                    best_move = move
-                beta = min(beta, min_score)
-                if beta <= alpha:
-                    break
-            return best_move, min_score
-        else:
-            # Jogador 2 é egoísta
-            max_score = float('-inf')
-            best_move = None
-            valids = get_valid_moves(state, is_cooperative)
-            for move in valids:
-                new_state = make_move(state, move)
-                _, score = minimax(new_state, depth-1, alpha, beta, True, is_cooperative)
-                if score > max_score:
-                    max_score = score
-                    best_move = move
-                alpha = max(alpha, max_score)
-                if beta <= alpha:
-                    break
-            return best_move, max_score
+    
+    valores = []
+    for validos in final:
+        pos, val = validos
+        movimentosValidos.remove(pos)
+        valores.append(val['cor'])
+    
+    for color in opcoesCores:
+        if color not in valores:
+            mvVal = random.choice(movimentosValidos)
+            final.append((mvVal, {'cor': color, 'valor': 1}))
+
+    return final
+
+coresProximoMovimento(tab3, 'V')
+
+
+from itertools import permutations
+
+def permutate_colors(tab):
+    empty_cells = []
+    for i in range(len(tab)):
+        for j in range(len(tab[0])):
+            if tab[i][j] == 0:
+                empty_cells.append((i,j))
+    
+    permutations_list = []
+    for combination in permutations(empty_cells, 3):
+        permutation = []
+        for cell in combination:
+            permutation.append((cell, 'R'))
+            permutation.append((cell, 'B'))
+            permutation.append((cell, 'Y'))
+        permutations_list.append(permutation)
+    
+    return permutations_list
+
+
+tab3 = [    [{'cor': 'V', 'valor' : 1}, {'cor': 'A', 'valor' : 2},  {'cor': 'C', 'valor' : 1}, {'cor': 'V', 'valor' : 9}],
+    [0, 0, 0,0],
+    [0, 0, 0,0],
+    [0, 0, 0,{'cor': 'C', 'valor' : 1}],
+]
+
+permutations_list = permutate_colors(tab3)
+print(len(permutations_list))
